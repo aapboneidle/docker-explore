4d37bbf37f582339e85c6a048909f8dfac6ebc4e
Andy Pryor <andy.pryor@sea.co.uk>
2018-07-31 07:31:06 +0100

    Builds and runs on Windows docker (no nuget yet)

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -6,8 +6,8 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new MyFunction.MyFunction().Execute("Hello, World!"));
-            Console.ReadKey();
+            // Console.WriteLine(new MyFunction.MyFunction().Execute("Hello, World!"));
+            Console.WriteLine("Hello, World!");
         }
     }
 }
