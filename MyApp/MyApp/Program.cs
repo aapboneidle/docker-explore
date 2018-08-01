da291dde710cd2fbc2d954c24641ace2f4eae9a6
Andy Pryor <aap@dtes.tfl.gov.uk>
2018-08-01 12:44:44 +0100

    remove readkey

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -7,7 +7,6 @@ namespace MyApp
         static void Main(string[] args)
         {
             Console.WriteLine(new MyFunction.MyFunction().Execute("Hello, World!"));
-            Console.ReadKey();
         }
     }
 }
